eb36926b82dae14d15c204d73016f491101ed835
KamCy Ahaka <kamcyahaka@gmail.com>
2023-08-18 02:30:16 +0100

    🐛 [FIX]: Fixed overflowing bug in CallToAction

--- components/CallToAction.vue
+++ components/CallToAction.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="py-2.5 px-16 rounded-full font-bold"
+    class="py-2.5 w-full rounded-full font-bold"
     :class="props.isColored ? 'bg-green-950 text-white border-green-950 border' : 'text-green-950 border-green-950 border'"
   >
     {{ props.buttonText }}
